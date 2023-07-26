f7840975d08ad6fdf35a4b89c9ff17a0b25398f8
Reinhardtvbm <reinhardtvbm@gmail.com>
2023-07-26 16:08:54 +0200

    started with motor class

--- MARV_ESP32/motor.cpp
+++ MARV_ESP32/motor.cpp
@@ -1,1 +1,13 @@
 #include "motor.h"
+#include <Arduino.h>
+
+Motor::Motor(int encoder_pin_A, int encoder_pin_B) {
+    this->encoder_pin_A = encoder_pin_A;
+    this->encoder_pin_B = encoder_pin_B;
+
+    this->encoder = Encoder(this->encoder_pin_A, this->encoder_pin_B);
+}
+
+void Motor::set_speed() {
+
+}
--- MARV_ESP32/motor.h
+++ MARV_ESP32/motor.h
@@ -1,7 +1,19 @@
 #pragma once 
 
+#include <stdint.h>
+#include <Encoder.h>
+
+/// One motor, including its encoder and controller
 class Motor {
     public:
+        Motor(int encoder_pin_A, int encoder_pin_B);
+        void set_speed();
 
     private:
+        int encoder_pin_A, encoder_pin_B;
+        uint32_t encoder_counts;
+        Encoder encoder;
+
+        uint16_t speed();
+
 };
